0be88ed999489b5ec7fda8db0d2b15ade037c5a6
auraalex21 <48390729+auraalex21@users.noreply.github.com>
2024-08-14 21:59:57 +0200

    edit index.js (opti en cours)

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 import TasksService from "./tasks.service.js";
-const tasks = [
-  { id: `task-1`, text: `Faire les courses`, done: false },
-  { id: `task-2`, text: `Faire le ménage`, done: false },
-  { id: `task-3`, text: `Faire la vaisselle`, done: false },
-];
+const tasks = [];
 
 const taskService = TasksService(tasks);
 
@@ -11,32 +7,109 @@ const list = document.querySelector(".list");
 const form = document.querySelector("form");
 const input = document.querySelector("form > input");
 
-for (let index = 0; index < tasks.length; index++) {
-  const li = document.createElement("li");
-  li.classList.add("tache");
-  const task = tasks[index];
-  const model = `
-    <div class="name-tache">
+function createtaskItem() {
+  for (let index = 0; index < tasks.length; index++) {
+    const exsitingLi = document.getElementById(`li-${tasks[index].id}`);
+    if (exsitingLi) {
+      
+      continue;
+    }
+    const li = document.createElement("li");
+    li.classList.add("tache");
+    li.id = `li-${tasks[index].id}`;
+    const task = tasks[index];
+    const model = `
+    <div class="name-tache" id=name-tache-${task.id}>
       <span class="todo ${task.done ? "done" : "done"}"></span>
       <h1 class="todo done">${task.text}</h1>
+    </div>
+    <div class="emojis">
+      <div class="trash" id=trash-${task.id}>
+        <img src="./img/trash.svg"/>
       </div>
-      <div class="emojis">
-        <div class="trash">
-          <img src="./img/trash.svg"/>
-        </div>
-        <div class="edit">
-          <img src="./img/edit.svg"/>
-        </div>
+      <div class="edit" id=edit-${task.id}>
+        <img src="./img/edit.svg"/>
+      </div>
     </div>
   `;
-  li.innerHTML = model;
-  list.appendChild(li);
+    li.innerHTML = model;
+    list.appendChild(li);
+    li.querySelector(`#edit-${task.id}`).addEventListener("click", (event) =>
+      handleEditTask(event, task.id)
+    );
+    li.querySelector(`#trash-${task.id}`).addEventListener("click", (event) =>
+      handleRemoveTask(event, task.id)
+    );
+  }
+}
+
+function editTaskItem(event, id) {
+  event.stopPropagation();
+  const input = document.getElementById(`input-${id}`);
+
+  if (input === null) {
+    console.log("edit")
+    editItem(event, id);
+  } else {
+    console.log("save")
+    saveItem(event, id);
+  }
+}
+
+//cibler le name tache
+//suprimer le contenue
+//ajouter un input
+
+function editItem(event, id) {
+  const div = document.getElementById(`name-tache-${id}`);
+  div.innerHTML = "";
+  const input = document.createElement("input");
+  const task = taskService.getTaskById(id);
+  input.value = task.text;
+  input.id = `input-${id}`;
+  div.appendChild(input);
+}
+
+function saveItem(event, id) {
+  event.stopPropagation();
+
+  const input = document.getElementById(`input-${id}`);
+  const newText = input.value.trim();
+
+  if (newText) {
+    taskService.editTaskById(id, newText, taskService.getTaskById(id).done);
+    document.getElementById(`li-${id}`).remove();
+    createtaskItem()
+  }
+
+}
+
+function handleCreateTask(task) {
+  taskService.addTaskById(task);
+  createtaskItem();
+}
+
+function handleEditTask(event, id) {
+  editTaskItem(event, id);
+}
+
+//removeTask
+function removeTaskItem(event, id) {
+  event.stopPropagation();
+  console.log(id)
+  document.getElementById(`li-${id}`).remove();
+}
+
+function handleRemoveTask(event, id) {
+  removeTaskItem(event, id);
+  taskService.deleteTaskById(id);
+  console.log(tasks);
 }
 
 form.onsubmit = (e) => {
   e.preventDefault();
   const value = input.value;
-  taskService.addTaskById({
+  handleCreateTask({
     id: `task-${tasks.length + 1}`,
     text: value,
     done: false,
