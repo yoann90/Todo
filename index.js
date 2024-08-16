ee08146fd69a1d38ecb6f3fd471d34dfa8f67367
julien <juliensibue@outlook.fr>
2024-08-16 11:26:50 +0200

    edit index.js opti pour eviter le rerender de chaque task

--- index.js
+++ index.js
@@ -9,74 +9,89 @@ const input = document.querySelector("form > input");
 function handleTaskAction(id, action) {
   if (action === "create") {
     taskService.addTaskById(id);
+    renderTask(taskService.getTaskById(id.id));
   } else if (action === "edit") {
     const task = taskService.getTaskById(id);
-    const div = document.getElementById(`name-tache-${id}`);
     const inputEl = document.getElementById(`input-${id}`);
     if (inputEl) {
       const newText = inputEl.value.trim();
       if (newText) {
         taskService.editTaskById(id, newText, task.done);
-        div.innerHTML = renderTaskContent(id, newText, task.done);
+        document.getElementById(`name-tache-${id}`).innerHTML =
+          renderTaskContent(id, newText, task.done);
+        reattachEventListeners(id);
       }
     } else {
-      div.innerHTML = `<input id="input-${id}" value="${task.text}">`;
+      document.getElementById(
+        `name-tache-${id}`
+      ).innerHTML = `<input id="input-${id}" value="${task.text}">`;
     }
-    return;
   } else if (action === "remove") {
     document.getElementById(`li-${id}`).remove();
     taskService.deleteTaskById(id);
-    return;
   } else if (action === "toggleDone") {
     const task = taskService.getTaskById(id);
     taskService.editTaskById(id, task.text, !task.done);
+    document.getElementById(`name-tache-${id}`).innerHTML = renderTaskContent(
+      id,
+      task.text,
+      !task.done
+    );
+    reattachEventListeners(id);
   }
-  renderTasks();
+}
+
+function renderTask(task) {
+  const li = document.createElement("li");
+  li.className = "tache";
+  li.id = `li-${task.id}`;
+  li.innerHTML = `
+    <div class="name-tache" id="name-tache-${task.id}">
+      ${renderTaskContent(task.id, task.text, task.done)}
+    </div>
+    <div class="emojis">
+      <div class="trash" id="trash-${task.id}">
+        <img src="./img/trash.svg"/>
+      </div>
+      <div class="edit" id="edit-${task.id}">
+        <img src="./img/edit.svg"/>
+      </div>
+    </div>
+  `;
+  list.appendChild(li);
+  li.querySelector(`#span-${task.id}`).addEventListener("click", () =>
+    handleTaskAction(task.id, "toggleDone")
+  );
+  li.querySelector(`#edit-${task.id}`).addEventListener("click", () =>
+    handleTaskAction(task.id, "edit")
+  );
+  li.querySelector(`#trash-${task.id}`).addEventListener("click", () =>
+    handleTaskAction(task.id, "remove")
+  );
 }
 
 function renderTasks() {
   list.innerHTML = "";
-  tasks.forEach((task) => {
-    const li = document.createElement("li");
-    li.className = "tache";
-    li.id = `li-${task.id}`;
-    li.innerHTML = `
-      <div class="name-tache" id="name-tache-${task.id}">
-        ${renderTaskContent(task.id, task.text, task.done)}
-      </div>
-      <div class="emojis">
-        <div class="trash" id="trash-${task.id}">
-          <img src="./img/trash.svg"/>
-        </div>
-        <div class="edit" id="edit-${task.id}">
-          <img src="./img/edit.svg"/>
-        </div>
-      </div>
-    `;
-    list.appendChild(li);
-    li.querySelector(`#span-${task.id}`).addEventListener("click", () =>
-      handleTaskAction(task.id, "toggleDone")
-    );
-    li.querySelector(`#edit-${task.id}`).addEventListener("click", () =>
-      handleTaskAction(task.id, "edit")
-    );
-    li.querySelector(`#trash-${task.id}`).addEventListener("click", () =>
-      handleTaskAction(task.id, "remove")
-    );
-  });
+  tasks.forEach(renderTask);
 }
 
 function renderTaskContent(id, text, done) {
-  return `<span id = span-${id} class="todo ${
+  return `<span id="span-${id}" class="todo ${
     done ? "done" : ""
   }"></span><h1 class="${done ? "done" : ""}">${text}</h1>`;
 }
 
+function reattachEventListeners(id) {
+  document
+    .querySelector(`#span-${id}`)
+    .addEventListener("click", () => handleTaskAction(id, "toggleDone"));
+}
+
 form.onsubmit = (e) => {
   e.preventDefault();
-  handleTaskAction(
-    { id: `task-${Date.now()}`, text: input.value, done: false },
-    "create"
-  );
+  const taskId = `task-${Date.now()}`;
+  handleTaskAction({ id: taskId, text: input.value, done: false }, "create");
   input.value = "";
 };
+
+renderTasks();
